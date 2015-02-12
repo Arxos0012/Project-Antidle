a991ace7f740540b796eeaa29796840738abcdc7
Jabari Booker <booker.oronde@gmail.com>
2015-02-12 00:55:59 -0500

    Started basis of the main class

--- Project Antidle/main.cpp
+++ Project Antidle/main.cpp
@@ -2,6 +2,87 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+bool init();		//initalizes all SDL stuff
+bool loadMedia();	//loads all assets
+void close();		//frees up all memory at the end
+
+SDL_Window* gWindow = NULL;
+SDL_Renderer* gRenderer = NULL;
+
 int main(int argc, char* argv[]){
+	if (!init()){
+		std::cerr << "Falied to initialize!\n";
+	}
+	else if (!loadMedia()){
+		std::cerr << "Falied to load media!\n";
+	}
+	else{
+		bool quit = false;
+		SDL_Event e;
+
+		while (!quit){
+			while (SDL_PollEvent(&e) != 0){
+				if (e.type == SDL_QUIT){
+					quit = true;
+				}
+			}
+		}
+	}
+
 	return 0;
+}
+
+bool init(){
+	bool success = true;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0){
+		printf("Failed to initialze SDL! SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else{
+		gWindow = SDL_CreateWindow("Project Antidle", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL){
+			printf("Failed to create game window! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else{
+			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+			if (gRenderer == NULL){
+				printf("Failed to create game renderer! SDL_Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			else{
+				SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0xFF);
+
+				int imgFlags = IMG_INIT_PNG;
+				if (!(IMG_Init(imgFlags) & imgFlags)){
+					printf("SDL_image failed to initialize: SDL_image Error: %s\n", IMG_GetError());
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+bool loadMedia(){
+	bool success = true;
+
+	return success;
+}
+
+void close(){
+
+	SDL_DestroyRenderer(gRenderer);
+	gRenderer = NULL;
+
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+	
+	IMG_Quit();
+	SDL_Quit();
 }
